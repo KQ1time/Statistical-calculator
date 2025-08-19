2b1e5af55488d808f9cde5ff1302c5069a5822d8
Alexander <k1time@mail.ru>
2025-08-19 18:55:05 +0300

    Update and rename statistical_cals.py to statistical_calc.py
    Улучшение интерфейса, исправление ошибки из-за которой числа больше 9 обрабатывались неверно.

--- statistical_calc.py
+++ statistical_calc.py
@@ -3,38 +3,49 @@
 import numpy as np
 
 # Входные данные
-data = input("Введи числовой набор через пробелы \n").split(sep=" ")
+data = input("Введите числовой набор через пробелы \n").split()
+
+# Обработка
+new_data = []
+
+for el in data:
+    if el != " ":
+        new_data.append(el)
 
 # Преобразуем все элементы списка в вещественные числа
-numbers = np.array(data, dtype=float)
+numbers = np.array(new_data, dtype=float)
 
 # Основная логика программы
 while True:
+    print("\n ----------Statistical calculator version 0.0.1---------- \n")
     print("Вы хотите продолжить?")
     calc_continue = input('Введите "да" или "нет" \n').lower()
 
     if calc_continue == "нет":
         break
     elif calc_continue == "да":
-        print("Выберите статистическую метрику из списка:")
+        print("\n Выберите статистическую метрику из списка:")
         metric = input("""медиана, среднее, максимум, минимум, размах, дисперсия, стандартное отклонение \n""")
         metric = metric.lower()
 
         if metric == "медиана":
+            print()
             print(f"Медиана: {np.median(numbers)}")
         elif metric == "среднее":
+            print()
             print(f"Среднее: {np.mean(numbers)}")
         elif metric == "максимум":
+            print()
             print(f"Максимум: {np.max(numbers)}")
         elif metric == "минимум":
+            print()
             print(f"Минимум: {np.min(numbers)}")
         elif metric == "размах":
+            print()
             print(f"Размах: {np.max(numbers) - np.min(numbers)}")
         elif metric == "дисперсия":
+            print()
             print(f"Дисперсия: {np.var(numbers)}")
         elif metric == "стандартное отклонение":
+            print()
             print(f"Стандартное отклонение: {np.std(numbers)}")
-
-        continue
-    else:
-        continue
